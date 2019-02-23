51f96024d3fd74573776892186d02711e17c3621
Anna Pazola <pazolka@gmail.com>
2019-02-23 02:06:39 +0100

    disable logging

--- src/common/redux/logs/reducers.js
+++ src/common/redux/logs/reducers.js
@@ -6,15 +6,15 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case Logs.ADD_LOG_LINES:
-      var time = new Date();
-      var newLines = action.lines.map(line => {
-        return { time: time, line: line };
-      });
+    // case Logs.ADD_LOG_LINES:
+    //   var time = new Date();
+    //   var newLines = action.lines.map(line => {
+    //     return { time: time, line: line };
+    //   });
 
-      return Object.assign({}, state, {
-        lines: state.lines.concat(newLines),
-      });
+    //   return Object.assign({}, state, {
+    //     lines: state.lines.concat(newLines),
+    //   });
 
     case Logs.CLEAR_LOG_LINES:
       return Object.assign({}, state, {
